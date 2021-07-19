cb62f727720d3d2c640babc9390a80ae7c251f8a
Michael Liu <thinker1990@users.noreply.github.com>
2021-07-19 15:28:43 +0800

    Exercises 1

---
+++ Chapter4/4_01.rkt
@@ -0,0 +1,3 @@
+;; The result would been 0. 
+;; Because counter rebinds to a new location that has the value 0 every time g is called, 
+;; the final value that referenced by counter will be the same.
